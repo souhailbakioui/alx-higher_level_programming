829b3db51126cceee8a08262621ea67a2974ea19
souhailbakioui <bakiouisohail@gmail.com>
2024-01-08 23:04:14 +0100

    add 0-main.py

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,8 @@
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{}".format(num))
+
+# Example usage:
+my_integer_list = [1, 2, 3, 4, 5]
+print_list_integer(my_integer_list)
+
