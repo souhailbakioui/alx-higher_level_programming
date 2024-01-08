33997266c28f1275c00393aef25e90cbd941a9fa
souhailbakioui <bakiouisohail@gmail.com>
2024-01-08 23:09:56 +0100

    add namesapce python to file

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
-
-# Example usage:
-my_integer_list = [1, 2, 3, 4, 5]
-print_list_integer(my_integer_list)
-
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
