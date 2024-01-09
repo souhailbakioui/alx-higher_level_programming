c7534ba42520b4ec56a6ec06ca6ac77aff4a599a
Bakioui Souhail <58438247+souhailbakioui@users.noreply.github.com>
2024-01-09 22:42:53 +0100

    Create 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    # Delete the item at the specified position without using pop()
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
