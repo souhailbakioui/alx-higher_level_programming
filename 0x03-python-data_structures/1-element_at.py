587b901b2bf9e85bd16c93bd51e58fcd461af67b
souhailbakioui <bakiouisohail@gmail.com>
2024-01-09 20:54:46 +0100

    add 2-replace_in_list.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 def element_at(my_list, idx):
     if len(my_list) <= idx or idx < 0:
         return None
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if len(my_list)<=idx or idx <0:
+        return None
+    else:
+        my_list[idx] = element
+        return my_list
