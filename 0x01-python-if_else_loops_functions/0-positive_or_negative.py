8b09a5685bf615d70b2ce94edd0fbfed421b7f23
souhailbakioui <bakiouisohail@gmail.com>
2024-01-19 12:16:28 +0100

    try to change format to linux

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,9 +4,9 @@ import random
 number = random.randint(-10, 10)
 print(number, end=' ')
 if number > 0:
-        print("is positive")
-    elif number == 0:
-          print("is zero")
-    else:
-          print("is negative")
+    print("is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
 
